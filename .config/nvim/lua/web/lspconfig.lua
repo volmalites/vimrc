242d3d2ac02423f7ab7f51eb0ebd2ab6ad140ea5
Bjorn Lottering <bjorn@octonet.co.za>
2022-12-21 17:16:32 +0200

    Deprecation fix

--- .config/nvim/lua/web/lspconfig.lua
+++ .config/nvim/lua/web/lspconfig.lua
@@ -46,7 +46,7 @@ cmp.setup.cmdline(':', {
   })
 })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 for _, server in ipairs(servers) do
   require('lspconfig')[server].setup {
