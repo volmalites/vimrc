6a27270228e43e76ec34d25a9689b1d58e4ee380
Bjorn Lottering <bjorn@octonet.co.za>
2023-02-16 21:55:21 +0200

    Migrating to packer

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,18 @@
+local use = require('packer').use
+
+require('packer').startup(function()
+	use 'wbthomason/packer.nvim'
+
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'hrsh7th/nvim-cmp'
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use 'hrsh7th/vim-vsnip'
+	use 'hrsh7th/vim-vsnip-integ'
+	use { 'neoclide/coc.nvim', branch = 'release' }
+	use 'simrat39/rust-tools.nvim'
+	use 'williamboman/mason.nvim'
+end)
