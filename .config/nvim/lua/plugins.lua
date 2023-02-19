8112f0e59d9e4065bc4ce581d17125bf655d9afc
Bjorn Lottering <bjorn@octonet.co.za>
2023-02-19 19:26:17 +0200

    Fix for TS migrating from vimplug, added melange theme

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -9,10 +9,18 @@ require('packer').startup(function()
 	use 'hrsh7th/cmp-path'
 	use 'hrsh7th/cmp-cmdline'
 	use 'hrsh7th/nvim-cmp'
-	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	--use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
 	use 'hrsh7th/vim-vsnip'
 	use 'hrsh7th/vim-vsnip-integ'
 	use { 'neoclide/coc.nvim', branch = 'release' }
 	use 'simrat39/rust-tools.nvim'
 	use 'williamboman/mason.nvim'
+	use 'savq/melange-nvim'
 end)
