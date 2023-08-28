1bd45c298d113ab3a05c36e359cc740e278f75e6
Bjorn Lottering <bjorn@octonet.co.za>
2023-08-28 14:38:15 +0200

    Update init.lua
    Only names in tablines

--- .config/nvim/lua/web/init.lua
+++ .config/nvim/lua/web/init.lua
@@ -4,3 +4,22 @@ require('web.ts')
 require('web.rust')
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
+vim.o.tabline = [[%!v:lua.MyTabLine()]]
+function MyTabLine()
+    local s = ''
+    for i=1,vim.fn.tabpagenr('$') do
+        -- Get the buffer name of the tab
+        local buflist = vim.fn.tabpagebuflist(i)
+        local bufnr = buflist[vim.fn.tabpagewinnr(i)]
+        local bufname = vim.fn.bufname(bufnr)
+
+        -- Extract only the file name from the buffer name
+        local filename = vim.fn.fnamemodify(bufname, ':t')
+
+        -- Build the tabline entry
+        s = s .. '%' .. i .. 'T'
+        s = s .. (i == vim.fn.tabpagenr() and '%#TabLineSel#' or '%#TabLine#')
+        s = s .. ' ' .. filename .. ' '
+    end
+    return s
+end
